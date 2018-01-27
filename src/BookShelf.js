e2f7029eacc74548aa3eb2348f69c448758e9492
Nick <ghosty190@hotmail.com>
2018-01-27 12:30:19 +0200

    feat: Implement search and update functionality

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,18 +3,19 @@ import './App.css'
 
 class BookShelf extends Component {
     render() {
+        const { books, setBookShelf } = this.props
         return (
             <div className="bookshelf">
                 {this.props.title && <h2 className="bookshelf-title">{this.props.title}</h2>}
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {this.props.books && this.props.books.map((book, i) => (
-                            <li>
+                        {this.props.books && books.map((book, i) => (
+                            <li key={i}>
                                 <div className="book">
                                     <div className="book-top">
                                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                                         <div className="book-shelf-changer">
-                                            <select>
+                                            <select value={book.shelf} onChange={(event) => setBookShelf(book, event.target.value)}>
                                                 <option value="none" disabled>Move to...</option>
                                                 <option value="currentlyReading">Currently Reading</option>
                                                 <option value="wantToRead">Want to Read</option>
---
+++ src/SearchBooks.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookShelf'
+import './App.css'
+
+class SearchBooks extends Component {
+    state = {
+        queryBooks: []
+    }
+
+    searchQuery = (query) => {
+        BooksAPI.search(query).then(queryBooks => {
+            queryBooks && queryBooks.length > 0 && this.setState({
+                queryBooks: queryBooks.map((queryBook) => {
+                    var book = this.props.books.find((book) => book.id === queryBook.id)
+                    queryBook.shelf = book ? book.shelf : 'none'
+                    return queryBook
+                })
+            })
+        })
+    }
+
+    render() {
+        const { setBookShelf } = this.props
+        return (
+            <div className="app">
+                <div className="search-books">
+                    <div className="search-books-bar">
+                        <Link className="close-search" to="/">Back </Link>
+                        <div className="search-books-input-wrapper">
+                            <input type="text" placeholder="Search by title or author"
+                                onChange={(event) => this.searchQuery(event.target.value)} />
+                        </div>
+                    </div>
+                    <div className="search-books-results">
+                        <BookShelf books={this.state.queryBooks} setBookShelf={setBookShelf} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBooks

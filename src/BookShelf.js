4faaaae3e8108cb2b73435bea68f2d65462cd416
Nick <ghosty190@hotmail.com>
2018-01-27 13:00:19 +0200

    refactor: Make components stateless

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
 import './App.css'
 
-class BookShelf extends Component {
-    render() {
-        const { books, setBookShelf } = this.props
-        return (
-            <div className="bookshelf">
-                {this.props.title && <h2 className="bookshelf-title">{this.props.title}</h2>}
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books && books.map((book, i) => (
-                            <li key={i}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                                        <div className="book-shelf-changer">
-                                            <select value={book.shelf} onChange={(event) => setBookShelf(book, event.target.value)}>
-                                                <option value="none" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                        </div>
+const BookShelf = ({title, books, setBookShelf}) => {
+    return (
+        <div className="bookshelf">
+            {title && <h2 className="bookshelf-title">{title}</h2>}
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books && books.map((book, i) => (
+                        <li key={i}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select value={book.shelf} onChange={(event) => setBookShelf(book, event.target.value)}>
+                                            <option value="none" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
                                     </div>
-                                    <div className="book-title">{book.title}</div>
-                                    <div className="book-authors">{book.author}</div>
                                 </div>
-                            </li>
-                        ))}
-                    </ol>
-                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.author}</div>
+                            </div>
+                        </li>
+                    ))}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,36 +3,32 @@ import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import './App.css'
 
-class ListBooks extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-    }
-
-    render() {
-        const { books, setBookShelf } = this.props
-        return (
-            <div className="app">
-                <div className="list-books">
-                    <div className="list-books-title">
-                        <h1>MyReads</h1>
-                    </div>
-                    <div className="list-books-content">
-                        {books && books.length > 0 ?
-                            <div>
-                                <BookShelf title='Currently Reading' books={books.filter(book => book.shelf === 'currentlyReading')} setBookShelf={setBookShelf} />
-                                <BookShelf title='Want to Read' books={books.filter(book => book.shelf === 'wantToRead')} setBookShelf={setBookShelf} />
-                                <BookShelf title='Read' books={books.filter(book => book.shelf === 'read')} setBookShelf={setBookShelf} />
-                            </div>
-                            : <div className='loader' />
-                        }
-                    </div>
-                    <div className="open-search">
-                        <Link to="/search" >Add a book</Link>
-                    </div>
+const ListBooks = ({ books, setBookShelf }) => {
+    return (
+        <div className="app">
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    {books && books.length > 0 ?
+                        <div>
+                            <BookShelf title='Currently Reading' books={books.filter(book => book.shelf === 'currentlyReading')} setBookShelf={setBookShelf} />
+                            <BookShelf title='Want to Read' books={books.filter(book => book.shelf === 'wantToRead')} setBookShelf={setBookShelf} />
+                            <BookShelf title='Read' books={books.filter(book => book.shelf === 'read')} setBookShelf={setBookShelf} />
+                        </div>
+                        : <div className='loader' />
+                    }
+                </div>
+                <div className="open-search">
+                    <Link to="/search" >Add a book</Link>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
+ListBooks.propTypes = {
+    books: React.PropTypes.array.isRequired,
 }
 
 export default ListBooks
